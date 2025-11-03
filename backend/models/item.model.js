8797bc326a0d0eae0fc5d740f21ba02412029e0a
Amritesh Raj <amriteshraj1798@gmail.com>
2025-11-03 10:28:35 +0530

    Implement shop and item model

---
+++ backend/models/item.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    // item kis shop ka hai ye reference hoga
+    shop: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Shop",
+    },
+    category: {
+      type: String,
+      enum: [
+        "Snacks",
+        "Main Course",
+        "Desserts",
+        "Pizza",
+        "Burgers",
+        "Sandwiches",
+        "South Indian",
+        "North Indian",
+        "Chinese",
+        "Fast Food",
+        "Others",
+      ],
+      required: true,
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    foodType: {
+      type: String,
+      enum: ["veg", "non veg"],
+      required: true,
+    },
+    rating: {
+      average: { type: Number, default: 0 }, //unka average rating
+      count: { type: Number, default: 0 }, //kitno logo ne rate kiya
+    },
+  },
+  { timestamps: true }
+);
+
+const Item = mongoose.model("Item", itemSchema);
+export default Item;
