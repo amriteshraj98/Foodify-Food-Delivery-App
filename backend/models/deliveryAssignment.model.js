582ed67e06d034fa067885177d8a57c9a701d252
Amritesh Raj <amriteshraj1798@gmail.com>
2026-02-09 21:24:26 +0530

    Refactor deliveryAssignment model definition

--- backend/models/deliveryAssignment.model.js
+++ backend/models/deliveryAssignment.model.js
@@ -2,35 +2,29 @@ import mongoose from "mongoose";
 
 const deliveryAssignmentSchema = new mongoose.Schema(
   {
-    //  kis order ke liye hai ye delivery bana hai 
     order: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Order",
     },
-    //  kis shop ka order hai ye 
     shop: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Shop",
     },
-    //   kis shop order(kadhai paneer) ke liye hai ye delivery 
     shopOrderId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
-    // kin kin delivery boys ko ye order bheja gya hai accept karne ke liye
     brodcastedTo: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
       },
     ],
-    // kis delivery boy ko ye order assign kiya gya hai
     assignedTo: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       default: null,
     },
-    // delivery  ka kya status hai
     status: {
       type: String,
       enum: ["brodcasted", "assigned", "completed"],
@@ -46,3 +40,4 @@ const DeliveryAssignment = mongoose.model(
   deliveryAssignmentSchema
 );
 export default DeliveryAssignment;
+
