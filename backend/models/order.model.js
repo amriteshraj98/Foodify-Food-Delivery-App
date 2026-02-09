3aaee990b0fe1ece796a8101c439475acf4b7b73
Amritesh Raj <amriteshraj1798@gmail.com>
2026-02-09 21:25:38 +0530

    Refactor order model schema for clarity

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const shopOrderItemSchema = new mongoose.Schema(
   {
-    // order kiya hua item kaun sa hai
     item: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Item",
@@ -17,32 +16,26 @@ const shopOrderItemSchema = new mongoose.Schema(
 
 const shopOrderSchema = new mongoose.Schema(
   {
-    //kis shop se order kiya hai kis shop ka name hai 
     shop: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Shop",
     },
-    // shop ka owner kaun hai
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-    // ek particular shop ka total order kitna hai
     subtotal: Number,
-    // ek shop ke andar kya kya items order kiye hain
     shopOrderItems: [shopOrderItemSchema],
     status: {
       type: String,
       enum: ["pending", "preparing", "out of delivery", "delivered"],
       default: "pending",
     },
-    // ye batayega ki is shop order kisko assign kiya gya hai delivery ke liye
     assignment: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "DeliveryAssignment",
       default: null,
     },
-    // delivery  kis delivery boy ko kiya gya hai
     assignedDeliveryBoy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
@@ -82,7 +75,6 @@ const orderSchema = new mongoose.Schema(
     totalAmount: {
       type: Number,
     },
-    // agar ek se zyada shops hain to unke orders ko alag alag rakhne ke liye array banaya user ek se zyada shops se order kar sakta hai 
     shopOrders: [shopOrderSchema],
 
     payment: {
@@ -103,3 +95,4 @@ const orderSchema = new mongoose.Schema(
 
 const Order = mongoose.model("Order", orderSchema);
 export default Order;
+
